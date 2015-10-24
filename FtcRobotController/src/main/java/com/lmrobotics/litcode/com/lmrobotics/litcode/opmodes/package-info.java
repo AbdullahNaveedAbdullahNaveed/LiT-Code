c510b0975b0956a7840cc83e2cd9fa9033e14bb5
Bryan Bettis <bryancbettis@gmail.com>
2015-10-24 14:29:25 -0500

    Started Setting Up Package Layout
    Added a couple of the packages and package-info files for future use.

---
+++ FtcRobotController/src/main/java/com/lmrobotics/litcode/com/lmrobotics/litcode/opmodes/package-info.java
@@ -0,0 +1,1 @@
+/** Contains all opmode classes made by our team. */
---
+++ FtcRobotController/src/main/java/com/lmrobotics/litcode/package-info.java
@@ -0,0 +1,3 @@
+/** Package containing all classes created or used by our FTC team (4324), other than the
+ * FTC robot controller libraries.
+ */
