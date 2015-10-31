5196a4558df006e521b204da60200a68aa0ee7e5
CursedNeon <CursedNeon@gmail.com>
2015-10-31 09:46:38 -0500

    Created Actions Class
    Created the Actions class.

---
+++ FtcRobotController/src/main/java/com/lmrobotics/litcode/autonomous/actions/Actions.java
@@ -0,0 +1,13 @@
+package com.lmrobotics.litcode.autonomous.actions;
+
+/**
+ *To complete events other than navigating around on the field.
+ * This can include simple actions, like moving a servo to a set
+ * position, or more complex actions, like taking sensor color
+ * readings and pressing the correct button on the beacon.
+ * Actions could involve giving Navigation an additional
+ * coordinate to move to, like moving to the left or right beacon
+ * button based on sensor results.
+ */
+public class Actions {
+}
