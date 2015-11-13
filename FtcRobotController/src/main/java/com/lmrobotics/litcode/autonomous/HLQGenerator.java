37ad9afc3a96884436cd20b5cdae7f6ec15d21ad
Bryan Bettis <bryancbettis@gmail.com>
2015-11-12 20:59:01 -0600

    HLQ Generator Work
    Started trying to implement reading of xml event config files to build
    the HLQ.

--- FtcRobotController/src/main/java/com/lmrobotics/litcode/autonomous/HLQGenerator.java
+++ FtcRobotController/src/main/java/com/lmrobotics/litcode/autonomous/HLQGenerator.java
@@ -1,6 +1,10 @@
 package com.lmrobotics.litcode.autonomous;
 
+import android.util.Xml;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import org.xmlpull.v1.XmlPullParser;
 
 /**
  * Created by Bryan on 11/7/2015.
@@ -14,7 +18,29 @@ public class HLQGenerator
      */
     public static ConcurrentLinkedQueue<EventBlock> makeHLQ(String configName)
     {
+        XmlPullParser parser = Xml.newPullParser();
+        try
+        {
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(in, null);
+            parser.nextTag();
+        }
+        catch (Exception e)
+        {
+            System.out.println("Unable to load event config file: '" + configName + "'");
+        }
+        return buildHLQ(readFeed(parser));
+    }
+
+    private static ConcurrentLinkedQueue<EventBlock> buildHLQ(List xmlData)
+    {
         ConcurrentLinkedQueue<EventBlock> newHLQ = new ConcurrentLinkedQueue<EventBlock>();
         return newHLQ;
+    }
+
+    private static List readFeed(XmlPullParser parser)
+    {
+        List entries = new ArrayList();
+        return entries;
     }
 }
