b8bb372e1aee70741e7663b8479b10b63607a59d
Bryan Bettis <bryancbettis@gmail.com>
2015-11-19 17:56:54 -0600

    Updated Drive System Setup
    Updated the DriveSystem to match the actual motor configuration.

--- FtcRobotController/src/main/java/com/lmrobotics/litcode/devices/DriveSystem.java
+++ FtcRobotController/src/main/java/com/lmrobotics/litcode/devices/DriveSystem.java
@@ -14,14 +14,12 @@ public class DriveSystem
     /** Normal constructor, needs the hardware map to get the motors. */
     public DriveSystem(HardwareMap hardwareMap)
     {
-        leftMotors = new DcMotor[3];
-        leftMotors[1] = hardwareMap.dcMotor.get("middleLeftDriveMotor");
-        leftMotors[2] = hardwareMap.dcMotor.get("frontLeftDriveMotor");
-        leftMotors[3] = hardwareMap.dcMotor.get("backLeftDriveMotor");
-        rightMotors = new DcMotor[3];
-        rightMotors[1] = hardwareMap.dcMotor.get("middleRightDriveMotor");
-        rightMotors[2] = hardwareMap.dcMotor.get("frontRightDriveMotor");
-        rightMotors[3] = hardwareMap.dcMotor.get("backRightDriveMotor");
+        leftMotors = new DcMotor[2];
+        leftMotors[0] = hardwareMap.dcMotor.get("frontLeftDrive");
+        leftMotors[1] = hardwareMap.dcMotor.get("backLeftDrive");
+        rightMotors = new DcMotor[2];
+        rightMotors[0] = hardwareMap.dcMotor.get("frontRightDrive");
+        rightMotors[1] = hardwareMap.dcMotor.get("backRightDrive");
     }
 
 
