e3b4bc0173fa08fb10322ee17378187bae1a55ee
Jack Studier <jackstudier@gmail.com>
2015-11-19 16:45:04 -0600

    Added Dead Band/Comments
    Added a dead band for joystick controls. Added comments for the
    functions to clean them up.

--- FtcRobotController/src/main/java/com/lmrobotics/litcode/teleop/opmodes/TeleOp.java
+++ FtcRobotController/src/main/java/com/lmrobotics/litcode/teleop/opmodes/TeleOp.java
@@ -8,6 +8,8 @@ import com.lmrobotics.litcode.devices.ArmSystem;
  */
 public class TeleOp extends OpMode
 {
+    private static final float deadBand = 0.07f;
+
     public DriveSystem drive;
 
     public ArmSystem arm;
@@ -17,21 +19,41 @@ public class TeleOp extends OpMode
     @Override
     public void init()
     {
-        drive = new DriveSystem(hardwareMap);
+//        drive = new DriveSystem(this.hardwareMap);
+//        arm = new ArmSystem(this.hardwareMap);
     }
 
     @Override
     public void loop()
     {
-        drive.setLeft(gamepad1.left_stick_y / scaleSpeed);
-        drive.setRight(gamepad1.right_stick_y / scaleSpeed);
-        if (gamepad1.a)
+        float leftDriveControl = gamepad1.left_stick_y;
+        float rightDriveControl = gamepad1.right_stick_y;
+        // Left and right drive motor control
+        if (Math.abs(leftDriveControl) >= deadBand)
         {
-            scaleSpeed =  2.0f;
+            drive.setLeft((int)(leftDriveControl / scaleSpeed));
         }
         else
         {
-            scaleSpeed = 1.0f;
+            drive.setLeft(0);
+        }
+        // To control the robot with the left stick
+        if (Math.abs(rightDriveControl)  >= deadBand)
+        {
+            drive.setRight((int)(rightDriveControl / scaleSpeed));
+        }
+        else
+        {
+            drive.setRight(0);
+        }
+        // To control the robot with the right stick
+        if (gamepad1.a)
+        {
+            scaleSpeed =  2;
+        }
+        else
+        {
+            scaleSpeed = 1;
         }
 //        if (gamepad1.right_trigger)
 //        {
