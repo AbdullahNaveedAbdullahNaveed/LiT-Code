bf68e75e44ebcc343b21b823a5481b4107d4c9b6
Bryan Bettis <bryancbettis@gmail.com>
2015-12-19 11:55:17 -0600

    Started Implementing HeadingSystem
    Started implementing a heading system for use with autonomous.

--- FtcRobotController/src/main/java/com/lmrobotics/litcode/devices/HeadingSystem.java
+++ FtcRobotController/src/main/java/com/lmrobotics/litcode/devices/HeadingSystem.java
@@ -1,6 +1,10 @@
 package com.lmrobotics.litcode.devices;
 
+import com.qualcomm.hardware.HiTechnicNxtGyroSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.LegacyModule;
 
 /** Provide a standard way to determine the direction the robot is facing.  This could
  * use a gyroscope, compass, another sensor, or a combination of sensors.  Regardless of
@@ -9,11 +13,32 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
  */
 public class HeadingSystem
 {
+    private static final long updateInterval = 100;
+    private static final double GYRO_OFFSET = 583;
+    private HiTechnicNxtGyroSensor gyro;
     private static int initialHeading = 0;
+    private static double heading = initialHeading;
+    private long lastUpdate = 0;
 
     /** Basic constructor, needs the hardware map to setup access to the used sensors. */
     public HeadingSystem(HardwareMap hardwareMap)
     {
+        gyro = (HiTechnicNxtGyroSensor) hardwareMap.gyroSensor.get("LGyro");
+        getHeading();
+    }
 
+    public synchronized double getHeading()
+    {
+        long currTime = System.currentTimeMillis();
+        if (currTime-lastUpdate >= updateInterval)
+        {
+            double adjustment = gyro.getRotation()-GYRO_OFFSET;
+            if (Math.abs(adjustment) > 1.5)
+            {
+                heading += (gyro.getRotation()-GYRO_OFFSET);
+            }
+            lastUpdate = currTime;
+        }
+        return heading;
     }
 }
